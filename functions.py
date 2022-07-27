08209c91a4c266280b778347d17d198cc384c55e
Allen Santos <araujosantosallen@gmail.com>
2022-07-27 14:50:09 -0400

    Def deleteRow, Interface options.
    Created an interface to look when inputing data. Other details that I also change to be more flexible. And I also added another function that allows you to delete the rows(it's nor working).

--- functions.py
+++ functions.py
@@ -4,30 +4,30 @@ import csv
 def createTABLE(curs):
     name = input("What is the name of the Table: ")
     coname = input("What is the name of the name of the first column: ")
-    type = input("Type of the first row: ")
-    curs.execute(f"""CREATE TABLE {name}({coname}{type})""")
+    typ = input("INTEGER: REAL: TEXT: BLOB: ")
+    curs.execute(f"""CREATE TABLE {name}({coname}{typ.upper()})""")
 
-#Create a new file
-def insertColumn(curs):
+#Create a Column
+def crateColumn(curs):
+    table = input("What is the table tha you want to add a Column?")
     colmn = input("What is the name of the column: ")
     typ = input("INTEGER: REAL: TEXT: BLOB: ")
-    curs.execute(f"ALTER TABLE inventory ADD COLUMN {colmn}{typ}")
+    curs.execute(f"ALTER TABLE {table} ADD COLUMN {colmn}{typ}")
 
 #Create a CSV file
 def createCSV(curs):
+    table = input("What is the name of the table that you want create a csv file with? ")
     with open('data.csv', 'w') as csvfile:
         filewrite = csv.writer(csvfile, delimiter=',')
-        for row in curs.execute("SELECT * FROM inventory"):
+        for row in curs.execute(f"SELECT * FROM {table}"):
             li = []
             li.clear()
-            li.append(str(row))
+            li.append(row)
             filewrite.writerow(row)
 
-
-
-
 #Adds another row
-def adds(table):
+def addRow(table):
+    table = input("What is the table's name that you want to add another row? ")
     brand = input("Brand = ")
     item = input("Item = ")
     quantity = input("Quantity = ")
@@ -36,12 +36,21 @@ def adds(table):
     url = input("URL = ")
     id = int(input("ID = "))
     note = input("Note =  ")
-    table.execute(f"INSERT INTO inventory VALUES('{brand}', '{item}', '{quantity}', {price}, {total}, '{url}', '{note}', {id})")
+    table.execute(f"INSERT INTO {table} VALUES('{brand}', '{item}', '{quantity}', {price}, {total}, '{url}', '{note}', {id})")
 
+#Change any data in the table
+def deleteRow(table):
+    table = input("Which table? ")
+    item = input("What is the name of the item that you want to delete? ")
+    try:
+        table.execute(f"DELETE FROM {table} WHERE Brand = {item};")
+    except: 
+        print(f"Your item does no exist in the {table}")
 
 #Printing the dataTable
 def showTable(table):
-    for row in table.execute("SELECT * FROM inventory"):
+    table = input("Which table? ")
+    for row in table.execute(f"SELECT * FROM {table}"):
         for eachData in row:
             print(f"{eachData}", end=(" " * (5 - len(str(eachData)[-1]))))
         print("\n")
--- table.py
+++ table.py
@@ -3,16 +3,24 @@ import sqlite3, functions
 con = sqlite3.connect('dataTable.db')
 cur = con.cursor()
 
-#Creating a DataTable
-#function.createTABLE(cur)
-
-#functions.insertColumn(cur)
-
-#csv 
-functions.createCSV(cur)
-
-#Showing the table
-#functions.showTable(cur)
+#Menu
+while True:
+    print("( 1 ) Create a Table \n( 2 ) Create a new Colum \n( 3 ) Create a CSV file or uptade \n( 4 ) Add another row \n( 5 ) NONE \n( 6 ) Show table")
+    question = int(input(""))
+    if question == 1:
+        functions.createTABLE(cur)
+    elif question == 2:
+        functions.crateColumn(cur)
+    elif question == 3:
+        functions.createCSV(cur)
+    elif question == 4:
+        functions.addRow(cur)
+    elif question == 5:
+        pass
+    elif question == 6:
+        functions.showTable(cur)
+    else:
+        pass
 
 #Saving
 con.commit()
